a15efc77c1669ad6bdc282596be34324668df159
giles <gilesknap@gmail.com>
2021-03-25 07:43:31 +0000

    correct image repo path

--- kanikobuild.sh
+++ kanikobuild.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 # Script to be called by .gitlab-ci.yml to perform container build using gitlab kubernetes executor
 
-# this script is designed for projects below the group controls/containers
+# this script is designed for projects below the namespace <group name>/containers
+# and deploys images to:
+#     $CI_REGISTRY/$GROUP/work/$PROJECT_PATH for untagged commits
+#     $CI_REGISTRY/$GROUP/work/$PROJECT_PATH for tagged commits
+# where PROJECT_PATH is the namespace minus <group name>/containers
 
 echo 'Building image...'
-GROUP=`echo ${CI_PROJECT_NAMESPACE} | cut -f 1 -d "/"`
+GROUP=${CI_PROJECT_NAMESPACE%%/containers*}
+PROJECT_PATH=${CI_PROJECT_NAMESPACE##*containers/}
 
 if [ -z "${CI_COMMIT_TAG}" ]
 then
-  DESTINATION=$CI_REGISTRY/$GROUP/work/$CI_PROJECT_NAME
+  DESTINATION=$CI_REGISTRY/$GROUP/work/$PROJECT_PATH
   CI_COMMIT_TAG=$CI_COMMIT_REF_NAME
 else
-  DESTINATION=$CI_REGISTRY/$GROUP/prod/$CI_PROJECT_NAME
+  DESTINATION=$CI_REGISTRY/$GROUP/prod/$PROJECT_PATH
 fi
 
 CMD="/kaniko/executor --context $CI_PROJECT_DIR --dockerfile $CI_PROJECT_DIR/Dockerfile"
