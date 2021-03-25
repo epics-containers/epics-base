25162f0f80a492cde7cec122133829fda1b06396
giles <gilesknap@gmail.com>
2021-03-25 08:28:16 +0000

    fix comment

--- kanikobuild.sh
+++ kanikobuild.sh
@@ -5,8 +5,9 @@
 # and deploys images to:
 #     $PROJECT_PATH in the work registry for untagged commits
 #     $PROJECT_PATH in the prod registry for tagged commits
-# requires that CI_WORK_REGISTRY and CI_PROD_REGISTRY are set appropriately
-#     in Settings->CI->Variables in your gitlab group <group name>/containers
+# requires that CI_WORK_REGISTRY and CI_PROD_REGISTRY are set to a root path
+# in your image registry for each of work and production images.
+# See Settings->CI->Variables in the gitlab group <group name>/containers
 
 echo 'Building image...'
 GROUP=${CI_PROJECT_NAMESPACE%%/containers*}
