ea600b100e516b9f09818587c0b8aab361ad615c
Giles Knap <giles.knap@diamond.ac.uk>
2024-03-06 14:08:52 +0000

    use correct RTEMS arch check

--- epics/scripts/patch-epics-base.sh
+++ epics/scripts/patch-epics-base.sh
@@ -7,7 +7,7 @@ sed -i -E 's/(^[^#].*+= test.*$)/# \1/' \
     /epics/epics-base/Makefile \
     /epics/epics-base/modules/*/Makefile
 
-if [[ $TARGET_ARCHITECTURE == "rtems" ]]; then
+if [[ $TARGET_ARCHITECTURE == "RTEMS-beatnik" ]]; then
     echo "Configuring epics-base to build RTEMS beatnik"
 
     cp ${THIS_DIR}/rtems/CONFIG_SITE.local ${EPICS_BASE}/configure/CONFIG_SITE.local
