89f825dbfd73bd59ea04502ae192be936dfec8a7
Giles Knap <giles.knap@diamond.ac.uk>
2024-03-11 11:14:26 +0000

    add cross compile to config_site

--- epics/scripts/patch-epics-base.sh
+++ epics/scripts/patch-epics-base.sh
@@ -11,4 +11,10 @@ if [[ $TARGET_ARCHITECTURE == "RTEMS-beatnik" ]]; then
     echo "Configuring epics-base to build RTEMS beatnik"
 
     cp ${THIS_DIR}/rtems/CONFIG_SITE.local ${EPICS_BASE}/configure/CONFIG_SITE.local
+elif [[ $TARGET_ARCHITECTURE != "linux-x86_64" ]]; then
+    echo "Configuring epics-base for target ${TARGET_ARCHITECTURE}"
+
+    touch ${EPICS_BASE}/configure/CONFIG_SITE.local
+    echo CROSS_COMPILER_TARGET_ARCHS=${TARGET_ARCHITECTURE} >> \
+      ${EPICS_BASE}/configure/CONFIG_SITE.local
 fi
