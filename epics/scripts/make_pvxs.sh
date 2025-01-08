ad142e5c31f77f4c817faefa7a194736015dc238
Giles Knap <giles.knap@diamond.ac.uk>
2025-01-08 14:51:18 +0000

    dont build pvxs unless on Linux x86

--- epics/scripts/make_pvxs.sh
+++ epics/scripts/make_pvxs.sh
@@ -5,23 +5,25 @@
 
 VERSION=1.3.2
 NAME=pvxs
-FOLDER=$(dirname $(readlink -f $0))
 
+# Can't build for RTEMS because we need libevent-dev - how do we get that?
+# Can't build for aarch64 - I think the Makefile assumes that the perl
+# scripts exist in linux-x86_64 folders
+if [[ $EPICS_TARGET_ARCH == "linux-x86_64" ]]; then
+    # log output and abort on failure
+    set -xe
 
-# log output and abort on failure
-set -xe
+    cd ${SUPPORT}
+    git clone https://github.com/epics-base/pvxs.git -b ${VERSION} --depth 1 ${NAME}
+    cd ${NAME}
 
-cd ${SUPPORT}
-git clone https://github.com/epics-base/pvxs.git -b ${VERSION} --depth 1 ${NAME}
-cd ${NAME}
+    # don't build test folders
+    sed -i -E 's/(^[^#].*example)/# \1/' Makefile
+    sed -i -E 's/(^[^#].*test)/# \1/' Makefile
 
-# don't build test folders
-sed -i -E 's/(^[^#].*example)/# \1/' Makefile
-sed -i -E 's/(^[^#].*test)/# \1/' Makefile
+    # add in the global RELEASE file as
+    ln -s ${SUPPORT}/configure/RELEASE ./configure/RELEASE.local
 
-# add in the global RELEASE file as RELEASE.local
-ln -s ${SUPPORT}/configure/RELEASE ./configure/RELEASE.local
-
-make -j $(nproc)
-make clean
-
+    make -j $(nproc)
+    make clean
+fi
